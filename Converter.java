bad28617f20640ffef6f298ac9455dffacd9fe9d
Christian Fagherazzi <1820548@stud.hs-mannheim.de>
2022-08-21 17:31:45 +0200

    implements a Hashmap and a constructor

--- Converter.java
+++ Converter.java
@@ -1,11 +1,19 @@
-public class Converter {
-    double exchange_Rates = 1.4373;
-    public double convort(double converterMoney){
-        
-        return (converterMoney*exchange_Rates)/1;
-        
+import java.util.HashMap;
+
+public class Converter {    
+    HashMap<String, Double> exchange_Rates= new HashMap<String, Double>();
+    Converter(){
+        exchange_Rates.put("USB",1.4373);
+        exchange_Rates.put("JPY",115.20);
+        exchange_Rates.put("BGN",1.9558);
+        exchange_Rates.put("CZK",24.213);
     }
+
+    public double convort(double converterMoney, String currencie ){
+        return (converterMoney*exchange_Rates.get(currencie.toUpperCase()))/1;
+    }
+    
     public static void main(String[] args) {
-        System.out.println(new Converter().convort(10.00000));
+        System.out.println(new Converter().convort(14.00000,"jpy"));
     }
 }
