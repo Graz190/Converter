4df634b26bb516c4d2a6e1536b93b3c839020edd
Christian Fagherazzi <1820548@stud.hs-mannheim.de>
2022-08-21 17:34:49 +0200

    misspell currencie

--- Converter.java
+++ Converter.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class Converter {    
     HashMap<String, Double> exchange_Rates= new HashMap<String, Double>();
     Converter(){
-        exchange_Rates.put("USB",1.4373);
+        exchange_Rates.put("USD",1.4373);
         exchange_Rates.put("JPY",115.20);
         exchange_Rates.put("BGN",1.9558);
         exchange_Rates.put("CZK",24.213);
@@ -12,8 +12,8 @@ public class Converter {
     public double convort(double converterMoney, String currencie ){
         return (converterMoney*exchange_Rates.get(currencie.toUpperCase()))/1;
     }
-    
+
     public static void main(String[] args) {
-        System.out.println(new Converter().convort(14.00000,"jpy"));
+        System.out.println("Money Converted= "+new Converter().convort(14.00000,"usd"));
     }
 }
