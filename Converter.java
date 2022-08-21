643caa33202ec63916b4c4208ced5a789c87508d
Christian Fagherazzi <1820548@stud.hs-mannheim.de>
2022-08-21 17:00:37 +0200

    first commit

---
+++ Converter.java
@@ -0,0 +1,8 @@
+public class converter {
+    double exchange_Rates = 1.4373;
+    public double convort(double converterMoney){
+        
+        return (converterMoney*exchange_Rates)/1;
+        
+    }
+}
